343a7bb193d28e549cbb7c5793cf987799998aff
Arunbalaji Singaravelu <ArunS@keysoftwareinc.com>
2025-08-23 16:15:09 +0530

    Fixed Next.js version issues in deployement and font issues in layout.tsx

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // reactStrictMode: true,
+  // experimental: {
+  //   appDir: true, // ensures App Router works
+  // },
+};
+
+module.exports = nextConfig;
